04fe8602fcac5019a61b68194e46f42c1f59661b
Ankit Amonkar <ankit.amonkar@gmail.com>
2019-10-22 19:46:15 -0400

    First Commit

---
+++ src/main/java/frc/team4909/robot/Robot.java
@@ -0,0 +1,42 @@
+package frc.team4909.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the TimedRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+
+  @Override
+  public void robotInit(){
+
+  }
+
+  public void robotPeriodic(){
+
+  }
+
+  public void autonomousInit(){
+
+  }
+
+  public void autonomousPeriodic(){
+
+  }
+
+  public void teleopInit(){
+
+  }
+
+  public void teleopPeriodic(){
+
+  }
+
+  
+}
