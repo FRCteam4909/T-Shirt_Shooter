4105bcd6e7267bee5b68fe0b7fe3cbbf623ffda7
jasonbishop11 <jasonbishop11@gmail.com>
2019-11-19 18:24:27 -0500

    Changed Solenoid to use Motor Controller

--- src/main/java/frc/team4909/robot/shooter/ShooterSubsystem.java
+++ src/main/java/frc/team4909/robot/shooter/ShooterSubsystem.java
@@ -1,26 +1,27 @@
 package frc.team4909.robot.shooter;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.team4909.robot.shooter.commands.FillTank;
 
 public class ShooterSubsystem extends Subsystem{
     
     public static WPI_TalonSRX AngleMotor;
-    public static Solenoid solenoid;
+    public static WPI_VictorSPX solenoid;
 
     public ShooterSubsystem(){
         AngleMotor = new WPI_TalonSRX(5);
-        solenoid = new Solenoid(1);
+        solenoid = new WPI_VictorSPX(6);
     }
 
     public void openValve() {
-        solenoid.set(true);
+        solenoid.set(1);
     }
 
     public void closeValve(){
-        solenoid.set(false);
+        solenoid.set(0);
     }
 
     public void raiseShooter(){
