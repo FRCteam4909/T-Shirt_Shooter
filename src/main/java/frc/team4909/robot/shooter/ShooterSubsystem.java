821b86613609a9b88d62852a306d2478cab1d8e3
jasonbishop11 <jasonbishop11@gmail.com>
2019-11-19 18:29:29 -0500

    Changed Solenoid to Talon

--- src/main/java/frc/team4909/robot/shooter/ShooterSubsystem.java
+++ src/main/java/frc/team4909/robot/shooter/ShooterSubsystem.java
@@ -1,19 +1,16 @@
 package frc.team4909.robot.shooter;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
-import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
-import frc.team4909.robot.shooter.commands.FillTank;
 
 public class ShooterSubsystem extends Subsystem{
     
     public static WPI_TalonSRX AngleMotor;
-    public static WPI_VictorSPX solenoid;
+    public static WPI_TalonSRX solenoid;
 
     public ShooterSubsystem(){
         AngleMotor = new WPI_TalonSRX(5);
-        solenoid = new WPI_VictorSPX(6);
+        solenoid = new WPI_TalonSRX(6);
     }
 
     public void openValve() {
